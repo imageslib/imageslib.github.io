9724b37da4b3a951df3314d25f69f5d41711da28
msr <mshineray@qq.com>
2024-09-23 12:10:20 +0800

    deploy

--- src/theme/components/Home.vue
+++ src/theme/components/Home.vue
@@ -2,10 +2,32 @@
 // if(typeof location !== 'undefined'){
 //   location.replace('https://www.tobetter.org.cn')
 // }
+const imgs = import.meta.glob(
+    [
+      '/public/img/**/*.png',
+      '/public/img/**/*.svg',
+      '/public/img/**/*.jpeg',
+      '/public/img/**/*.jpg',
+    ],
+    {
+      eager: true,
+      as: 'url'// https://vitejs.cn/vite3-cn/guide/features.html#glob-import
+    })
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <fieldset
+      v-for="(img,idx) in imgs"
+     :key="img"
+      style="margin-bottom: 20px; padding: 20px"
+    >
+      <legend>
+        {{img.replace('/public', 'https://imageslib.github.io')}}
+      </legend>
+      <img :src="img"/>
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
